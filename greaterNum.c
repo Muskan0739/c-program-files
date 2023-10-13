8e00a82b725291e9066d5fc4494eaf961b73c62f
muskan shukla <muskanshukla707@gmail.com>
2023-10-13 20:15:43 +0530

    Program to find greater Number

---
+++ greaterNum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+    int a=5;
+    int b=4;
+
+    if (a>b){
+        printf("a is greater than b \n");
+    }
+    else{
+        printf("b is greater than a \n");
+    }
+}
